2d0479d8bb72a09c12b1fc8ebe5353f26bbbc5be
carcigenicate <slomifier@gmail.com>
2018-01-11 16:57:20 -0700

    Got it working.

--- src/sponge/menger/first_sponge.clj
+++ src/sponge/menger/first_sponge.clj
@@ -1,36 +1,42 @@
 (ns sponge.menger.first-sponge)
 
-(defrecord Tile [tl-x tl-y width])
+(defrecord Tile [tl-x tl-y width]
+  Object
+  (toString [self] (str (into {} self))))
 
 (defrecord Sponge [tiles])
-
 
 (defn subdivide-tile
   "Expects that side-divions is odd."
   [tile side-divions]
   (let [{:keys [tl-x tl-y width]} tile
         sub-width (double (/ width side-divions))]
-    (for [y (range tl-y (+ tl-y width) sub-width)
-          x (range tl-x (+ tl-x width) sub-width)]
+    (for [y (range tl-y (+ tl-y width) (inc sub-width))
+          x (range tl-x (+ tl-x width) (inc sub-width))]
       (->Tile x y sub-width))))
 
-(defn remove-center [subdivided-tiles]
-  ; Just change to vector and dissoc?
-  (let [sqr (-> subdivided-tiles (count) (Math/sqrt) (int))
-        middle (int (Math/floor (/ sqr 2)))
-        remove-i (+ (* middle sqr) middle)]
+(defn subdivide-tile-remove-center
+  "Expects that side-divions is odd."
+  [tile side-divions]
+  (let [{:keys [tl-x tl-y width]} tile
+        sub-width (double (/ width side-divions))]
+    (for [y (range tl-y (+ tl-y width) (inc sub-width))
+          x (range tl-x (+ tl-x width) (inc sub-width))]
+      (->Tile x y sub-width))))
+
+(defn remove-center [subdivided-tiles side-divisions]
+  (let [middle (int (/ side-divisions 2))
+        remove-i (+ (* middle side-divisions) middle)]
     (map second
       (filter #(not= (first %) remove-i)
               (map vector (range) subdivided-tiles)))))
 
 (defn full-menger-subdivide [tiles side-divisions]
-  (mapcat #(-> %
-               (subdivide-tile side-divisions)
-               (remove-center))
+  (mapcat #(remove-center (subdivide-tile % side-divisions) side-divisions)
           tiles))
 
 (defn iterate-menger-subdivide [tiles side-divisions iterations]
   (nth (iterate #(full-menger-subdivide % side-divisions) tiles)
-       iterations))
+       (inc iterations)))
 
 
--- src/sponge/menger/first_try.clj
+++ src/sponge/menger/first_try.clj
@@ -3,30 +3,62 @@
             [seesaw.graphics :as sg]
             [seesaw.dev :as sd]
             [seesaw.color :as s-col]
+            [seesaw.font :as sf]
 
             [sponge.menger.first-sponge :as sp]))
 
-(def screen-width 1800)
+; TODO: Base size on canvas size, not window size
+; TODO: map coordinates from sponge coord to window coord
 
-(def tiles (sp/iterate-menger-subdivide
-             [(sp/->Tile 0 0 screen-width)]
-             3 6))
+(def screen-width 1500)
 
-(defn paint [cvs g]
-  (let []
-    (doseq [{:keys [tl-x tl-y width]} tiles]
-      (sg/draw g
-        (sg/rect tl-x tl-y width)
-        (sg/style :background :red)))))
+(def default-subdivisions 3)
 
+(def control-font (sf/font :size 30))
 
-(defn new-canvas []
-  (let [canvas (sc/canvas :paint paint)]
+(defn paint [future-tiles-atom cvs g]
+  (let [fut? @future-tiles-atom]
+    (when (and fut? (realized? fut?))
+      (doseq [{:keys [tl-x tl-y width] :as tile} @fut?]
+        (sg/draw g
+          (sg/rect tl-x tl-y (+ width 2))
+          (sg/style :background :black))))))
+
+(defn future-tiles [iterations]
+  (future
+    (sp/iterate-menger-subdivide
+       [(sp/->Tile 0 0 screen-width)]
+       default-subdivisions
+       iterations)))
+
+(defn new-canvas [tiles-atom]
+  (let [canvas (sc/canvas :paint (partial paint tiles-atom))]
     canvas))
 
+(defn iterations-selector-controls [canvas tiles-atom]
+  (let [selector (sc/spinner :font control-font,
+                             :model (sc/spinner-model 1 :from 0, :to 7, :by 1))
+        sub-button (sc/button :text "Generate", :font control-font)]
+
+     (sc/listen sub-button
+       :action (fn [_]
+                 (swap! tiles-atom
+                        (fn [old-fut]
+                          (when old-fut
+                            (future-cancel old-fut))
+
+                          (let [new-fut (future-tiles (sc/value selector))]
+                            (sc/repaint! canvas)
+                            new-fut)))))
+
+     (sc/horizontal-panel :items [sub-button selector])))
+
 (defn new-main-panel []
-  (let [canvas (new-canvas)]
-    (sc/border-panel :center canvas)))
+  (let [future-tiles-atom (atom nil)
+        canvas (new-canvas future-tiles-atom)
+        controls (iterations-selector-controls canvas future-tiles-atom)]
+
+    (sc/border-panel :center canvas, :south controls)))
 
 (defn frame []
   (let [content (new-main-panel)
